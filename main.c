1d60b8983b72773662bfff90f60b0285cf48ed68
santxz45 <santos.cleldisson09@aluno.ifce.edu.br>
2024-11-18 07:26:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+#include <stdlib.h>
+int main()
+{
+    int n, *p;
+    scanf("%d", &n);
+    
+    if(n == 0){
+        printf("[vazio]");
+        return 0;
+    }
+    
+    p = (int *) malloc (n * sizeof(int));
+    
+    if(p == NULL){
+        printf("Erro ao alocar memória\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &p[i]);
+    }
+    
+    printf("[");
+    for(int i = 0; i < n; i++){
+        if(i > 0) printf(",");
+        printf("%d", p[i]);
+    }
+    
+    printf("]\n");
+    
+    free(p);
+    
     return 0;
 }
